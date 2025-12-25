30857f2c3736161ac0140f21ec713530e50f07e6
darshan-sharma4 <dksharma112004@gmail.com>
2025-12-25 23:16:51 +0530

    updated with new pattern questions

---
+++ D-4_patterns/hallow_diamond.cpp
@@ -0,0 +1,53 @@
+//    *
+//   * *
+//  *   *
+// *     *
+//  *   *
+//   * *
+//    *
+
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n = 4;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        if (i != 0)
+        {
+            for (int j = 0; j < 2 * i - 1; j++)
+            {
+                cout << " ";
+            }
+
+            cout << "*";
+        }
+        cout << endl;
+    }
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        for (int j = n - 2 * i - 1; j > 0; j--)
+        {
+            cout << " ";
+        }
+        if (i != 2)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
+
+// to generate odd number 2*i-1
---
+++ D-4_patterns/inverted_triangle.cpp
@@ -0,0 +1,20 @@
+// 1111
+//  222
+//   33
+//    4
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n =4;
+    for(int i=0; i<n; i++){
+        for(int k=0;k<i;k++){
+            cout<<" ";
+        }
+        for(int j=0; j<n-i; j++){
+        cout<<i+1;
+        }
+        cout <<endl;
+    }
+}
