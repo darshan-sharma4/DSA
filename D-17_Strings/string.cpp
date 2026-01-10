21a353027c6131efa29e39e247996454fc87e16c
darshan-sharma4 <dksharma112004@gmail.com>
2026-01-10 20:55:33 +0530

    updated

---
+++ D-17_Strings/string.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+#include <cstring>
+#include <string>
+
+
+using namespace std;
+
+int main(){
+    // char str[]="darshan";
+    // char str[100];
+
+    // cout << "Enter char array:";
+    //  cin>>str;
+    // cin.getline(str,100);
+    // cout<<str<<" ";
+    // cout<< strlen(str) << endl;
+
+
+    string str ;
+    cout<< "enter char:";
+    getline(cin,str);
+     cout<<str<<" ";
+     cout<< str.length() << endl;
+}
