c7f3c516f19c85f4b902417182ac59ffb27d1e70
darshan-sharma4 <dksharma112004@gmail.com>
2026-01-20 22:06:04 +0530

    updated

--- D-20_Matrix/diagonalSum.cpp
+++ D-20_Matrix/diagonalSum.cpp
@@ -2,19 +2,19 @@
 #include <vector>
 using namespace std;
 
-// int diagonalSum(int mat[][3],int row,int col){
-//     int diaSum=0;
-//     for(int i=0; i<row; i++){
-//         for(int j=0; j<col; j++){
-//             if(i==j){
-//                 diaSum+=mat[i][j];
-//             }
-//             else if(j==row-1-i){
-//                 diaSum+=mat[i][j];
-//             }
-//         }
-//     }
-//     return diaSum;
+int diagonalSum(int mat[][3],int row,int col){
+    int diaSum=0;
+    for(int i=0; i<row; i++){
+        for(int j=0; j<col; j++){
+            if(i==j){
+                diaSum+=mat[i][j];
+            }
+            else if(j==row-1-i){
+                diaSum+=mat[i][j];
+            }
+        }
+    }
+    return diaSum;
 // }
 
 
