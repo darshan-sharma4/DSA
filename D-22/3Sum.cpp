0e578ad88b955c41043140433d6636b1fcb275d5
darshan-sharma4 <dksharma112004@gmail.com>
2026-01-26 22:12:52 +0530

    updated

---
+++ D-22/3Sum.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+// Most optimize appraoch
+
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<vector<int>> ans;
+    sort(nums.begin(), nums.end());
+    for (int i = 0; i < n; i++)
+    {
+        int j = i + 1, k = n - 1;
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+
+        while (j < k)
+        {
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0)
+            {
+                j++;
+            }
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                ans.push_back({nums[i], nums[j], nums[k]});
+                j++, k--;
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
+            }
+        }
+    }
+    return ans;
+}
+
+// optimize
+vector<vector<int>> threeSum(vector<int> &nums)
+{
+    int n = nums.size();
+
+    set<vector<int>> uniqueSet;
+    // a+b+c =0
+    //  c= -a -b;
+    for (int i = 0; i < n; i++)
+    {
+        int tar = -nums[i];
+        set<int> s;
+        for (int j = i + 1; j < n; j++)
+        {
+            int toFind = tar - nums[j];
+
+            if (s.find(toFind) != s.end())
+            {
+                vector<int> trip = {nums[i], nums[j], toFind};
+                sort(trip.begin(), trip.end());
+
+                uniqueSet.insert(trip);
+            }
+            s.insert(nums[j]);
+        }
+    }
+    vector<vector<int>> ans(uniqueSet.begin(), uniqueSet.end());
+    return ans;
+}
+
+// brute - force
+//  vector<vector<int>> threeSum(vector<int> &nums){
+//      int n = nums.size();
+//      vector<vector<int>> ans;
+//      set<vector<int>>s ;
+//      for(int i =0 ; i<n; i++){
+//          for(int j =i+1; j<n ; j++){
+//              for(int k =j+1; k<n; k++){
+//                  if(nums[i]+nums[j]+nums[k]==0){
+//                      vector<int> trip ={nums[i],nums[j],nums[k]};
+//                      sort(trip.begin(),trip.end());
+//                      if(s.find(trip) == s.end()){
+//                          s.insert(trip);
+//                          ans.push_back(trip);
+//                      }
+//                  }
+//              }
+//          }
+//      }
+//      return ans;
+
+// }
+
+int main()
+{
+}
